61bdd77fe4fe5cae472a64c2a436f61582b43fc7
Fishhh <hubert.gluchowski19@gmail.com>
2023-01-30 00:21:54 +0100

    Install shell completions in nix package

--- flake.nix
+++ flake.nix
@@ -15,6 +15,18 @@
         packages.default = naersk-lib.buildPackage {
           src = ./.;
 
+          postInstall = ''
+            OUT_DIR="$(find target/release/build -type d -path "target/release/build/http-hammer-*/out")"
+            if [ "$(echo "$OUT_DIR" | wc -l)" -ne 1 ]; then
+              echo "error: Too many out dirs generated" >&2
+              return 1
+            fi
+
+            install -Dm644 "$OUT_DIR/completions/_http-hammer" "$out/share/zsh/site-functions/_http-hammer"
+            install -Dm644 "$OUT_DIR/completions/http-hammer.bash" "$out/share/bash-completion/completions/http-hammer"
+            install -Dm644 "$OUT_DIR/completions/http-hammer.fish" "$out/share/fish/vendor_completions.d/http-hammer.fish"
+          '';
+
           nativeBuildInputs = with pkgs; [ pkg-config ];
           buildInputs = with pkgs; [ openssl ];
         };
